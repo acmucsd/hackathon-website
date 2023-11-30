52ac2674aa51bf36cd28a3697490c45dc06c3239
Alex Zhang <alexzhang1618@gmail.com>
2023-11-30 13:51:40 -0800

    Update assets, spacing

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,18 +1,19 @@
 import styles from './socials.module.css';
+import Image from 'next/image';
 
 export default function Socials() {
   return (
     <div className={styles.socialsContainer}>
       <a className={styles.socialItem} href="https://acmurl.com/instagram" target="_blank">
-        <img src="./instagram.png" alt="Instagram Logo" className={styles.socialImage} />
+        <img src="./instagram.svg" alt="Instagram Logo" className={styles.socialImage} />
       </a>
 
       <a className={styles.socialItem} href="https://acmurl.com/facebook" target="_blank">
-        <img src="./facebook.png" alt="Facebook Logo" className={styles.socialImage} />
+        <img src="./facebook.svg" alt="Facebook Logo" className={styles.socialImage} />
       </a>
 
       <a className={styles.socialItem} href="https://acmurl.com/discord" target="_blank">
-        <img src="./discord.png" alt="Discord Logo" className={styles.socialImage} />
+        <img src="./discord.svg" alt="Discord Logo" className={styles.socialImage} />
       </a>
     </div>
   );
